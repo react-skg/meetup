2581896acdec9c566d1cb7e44ad8bb4f0002ed00
Sartios Savramis <sartios@ordereze.com>
2017-05-27 12:20:40 +0300

    create intro slide component

---
+++ src/presentations/reactDay/intro/components/resources/index.js
@@ -0,0 +1,19 @@
+const baseStyles = {
+  bgColor: '#7CDFF9',
+  italic:	true,
+  bold: true,
+  caps: true,
+  margin: "10px 10px 10px 10px",
+  padding: "10px 10px 10px 10px",
+  textColor: "#03171D",
+  textSize: "12px",
+  textAlign: "vertical",
+  textFont: "",
+  bgColor: "#7CDFF9",
+  bgImage: "",
+  bgDarken: "0.5"
+};
+
+export {
+  baseStyles
+};
