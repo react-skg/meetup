1d4192f3f4d7dcecac1eb97de1fc25e2e915b798
csoulioti <csoulioti@ordereze.com>
2017-06-06 15:38:44 +0300

    First commit

--- src/presentations/reactDay/stateManagement/stateManagement.jsx
+++ src/presentations/reactDay/stateManagement/stateManagement.jsx
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
 
-// import {
-//   Appear, BlockQuote, Cite, CodePane, Code, Deck, Fill, Fit,
-//   Heading, Image, Layout, ListItem, List, Quote, Slide, Text,
-// } from 'spectacle';
-
-import { Deck, Slide, Text, BlockQuote, Quote } from 'spectacle';
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  List,
+  ListItem,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Table,
+  TableRow,
+  TableHeaderItem,
+  TableItem,
+  Text
+} from "spectacle";
 
 import { theme } from "../common/themes/darkTheme.js";
+
+const slideTransition = ["slide"];
 
 export default class StateManagement extends Component {
   render() {
     return (
-      <Deck theme={theme} transition="slide" progress="pacman">
-        <Slide id="slide1">
-          <BlockQuote>
-              <Quote textColor="#FFF">"State Management"</Quote>
-          </BlockQuote>
-        </Slide>
-        <Slide id="slide2">
-          <Text>StateManagement 2</Text>
-        </Slide>
-        <Slide id="slide3">
-          <Text>StateManagement 3</Text>
-        </Slide>
-        <Slide id="slide4">
-          <Text>StateManagement 4</Text>
-        </Slide>
-      </Deck>
+      <Spectacle theme={theme}>
+        <Deck transition={slideTransition} transitionDuration={500} progress="pacman">
+          <Slide id="slide1">
+            <BlockQuote>
+                <Quote textColor="#FFF">"State Management"</Quote>
+            </BlockQuote>
+          </Slide>
+          <Slide id="slide2">
+            <Text>The Problem of State</Text>
+          </Slide>
+          <Slide id="slide3">
+            <Text>StateManagement 3</Text>
+          </Slide>
+          <Slide id="slide4">
+            <Text>StateManagement 4</Text>
+          </Slide>
+        </Deck>
+      </Spectacle>
     );
   }
 }
