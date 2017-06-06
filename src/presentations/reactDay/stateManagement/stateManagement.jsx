301a5c5fe51e1479b0f6e42bfab6b6ed1901a448
csoulioti <csoulioti@ordereze.com>
2017-06-06 16:17:41 +0300

    Add first slides for state management

--- src/presentations/reactDay/stateManagement/stateManagement.jsx
+++ src/presentations/reactDay/stateManagement/stateManagement.jsx
@@ -1,55 +1,77 @@
 import React, { Component } from 'react';
+import mapValues from "lodash/mapValues";
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
 
-// Import Spectacle Core tags
 import {
-  Appear,
-  BlockQuote,
-  Cite,
-  CodePane,
-  Deck,
-  Fill,
-  Heading,
-  Image,
-  Layout,
-  Link,
-  List,
-  ListItem,
-  Markdown,
-  Quote,
-  Slide,
-  Spectacle,
-  Table,
-  TableRow,
-  TableHeaderItem,
-  TableItem,
-  Text
-} from "spectacle";
+   Appear, BlockQuote, Cite, CodePane, Code, Deck, Fill, Fit,
+   Heading, Image, Layout, ListItem, List,Link, Quote, Slide, Text,
+} from 'spectacle';
 
-import { theme } from "../common/themes/darkTheme.js";
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+const theme = createTheme({
+  primary: "white",
+  secondary: "black",
+  tertiary: "#09b5c4",
+  quartenary: "rgba(255, 219, 169, 0.43)"
+}, {
+	primary: "Open Sans Condensed"
+});
 
 const slideTransition = ["slide"];
+
+const images = mapValues({
+  flux: require("./assets/flux.png")
+}, v => v.replace('/', ''));
+
+preloader(images);
 
 export default class StateManagement extends Component {
   render() {
     return (
-      <Spectacle theme={theme}>
-        <Deck transition={slideTransition} transitionDuration={500} progress="pacman">
-          <Slide id="slide1">
-            <BlockQuote>
-                <Quote textColor="#FFF">"State Management"</Quote>
-            </BlockQuote>
+      <Deck theme={theme} transition={slideTransition} transitionDuration={500} progress="pacman">
+        <Slide transition={slideTransition} bgColor="secondary">
+            <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
+              State Management in React
+            </Heading>
+        </Slide>
+
+        <Slide transition={slideTransition}>
+            <Heading size={1}>
+              The Problem of State
+            </Heading>
+            <List>
+              <Appear><ListItem>Application state, UI state</ListItem></Appear>
+              <Appear><ListItem>Where to push it?</ListItem></Appear>
+              <Appear><ListItem>How to manipulate it?</ListItem></Appear>
+              <Appear><ListItem>How to propagate changes to the UI?</ListItem></Appear>
+            </List>
+        </Slide>
+
+        <Slide transition={slideTransition}>
+            <Heading size={1}>
+              Solutions
+            </Heading>
+            <List>
+              <Appear><ListItem>Keep it all in React components (<b>App</b>?)</ListItem></Appear>
+              <Appear><ListItem><Link href="https://facebook.github.io/flux/">Flux</Link> - Actions, stores, dispatcher</ListItem></Appear>
+              <Appear><ListItem><Link href="http://redux.js.org/">Redux</Link> - Flux taken to bare minimum</ListItem></Appear>
+              <Appear><ListItem><Link href="https://mobxjs.github.io/mobx/">MobX</Link> - Observables, think spreadsheets</ListItem></Appear>
+            </List>
           </Slide>
-          <Slide id="slide2">
-            <Text>The Problem of State</Text>
+
+          <Slide transition={slideTransition}>
+              <Image src={images.flux} margin="40px auto" height="324px" />
           </Slide>
-          <Slide id="slide3">
-            <Text>StateManagement 3</Text>
+
+          <Slide transition={slideTransition} bgColor="secondary">
+            <Heading size={2} textColor="tertiary">
+              Code Sample
+            </Heading>
           </Slide>
-          <Slide id="slide4">
-            <Text>StateManagement 4</Text>
-          </Slide>
-        </Deck>
-      </Spectacle>
+      </Deck>
     );
   }
 }
