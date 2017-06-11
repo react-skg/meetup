1bcc59832fa25c1367fe499bec49da3fb599e331
Eleftherios Pegiadis <pegiadise@gmail.com>
2017-06-11 16:35:45 +0300

    Update second slide

--- src/presentations/reactDay/reactRouter/reactRouter.jsx
+++ src/presentations/reactDay/reactRouter/reactRouter.jsx
@@ -10,27 +10,72 @@ import { Deck, Slide, Text, Appear, List, ListItem, Heading } from 'spectacle';
 import { theme } from '../common/themes/darkTheme';
 
 export default class ReactRouter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+    };
+  }
+
   render() {
     return (
       <Deck theme={theme} transition="slide" progress="pacman">
         <Slide transition={['fade']} bgColor="primary">
-          <Heading>React Router v4</Heading>
-          <List>
+          <Heading><code>react-router</code> v4</Heading>
+          <List ordered>
             <Appear><ListItem>What is it. (Do I need it?)</ListItem></Appear>
             <Appear><ListItem>How we can use it.</ListItem></Appear>
-            <Appear><ListItem>Analyze all the different types of components that we can use for declaring routes.</ListItem></Appear>
+            <Appear>
+              <ListItem>
+                How to combine all the different types of components
+                that we can use for declaring routes.
+              </ListItem>
+            </Appear>
             <Appear><ListItem>Route auth, prompts and redirects.</ListItem></Appear>
-            <Appear><ListItem>How can I get the Router's params in a deeply nested component.</ListItem></Appear>
+            <Appear><ListItem>How we can get the {'Router\'s'} params in a deeply nested component.</ListItem></Appear>
             <Appear><ListItem>Animated route changing.</ListItem></Appear>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary">
-          <Heading>React Router v4</Heading>
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading textColor="#000" textSize="55px">
+            <code>react-router</code> / What is it?
+            </Heading>
           <List>
-            <Appear><ListItem>Industry standard for client side routing with React.</ListItem></Appear>
-            <Appear><ListItem>JSX and Virtual DOM</ListItem></Appear>
-            <Appear><ListItem>State & Events</ListItem></Appear>
-            <Appear><ListItem>Going Large Scale with React JS</ListItem></Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  Industry standard for client side routing with React.
+                </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  Its API changed in every major version, but in v4
+                  it makes sense because it follows the React {'component\'s'} lifecycle.
+                </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  We can achieve selective rendering of components by
+                  matching the path of each route.
+                </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  Restrict parts of our application and redirect
+                  unauthorized users.
+                </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  Prompt users when the route will change in order for them not to lose unsaved data.
+                </ListItem>
+            </Appear>
+            <Appear>
+              <ListItem textSize="35px">
+                  Code splitting. Incrementally
+                  download parts of our app, according to route matching.
+                  (<code>webpack</code> / <code>bundle-loader</code> or alternative)
+                </ListItem>
+            </Appear>
           </List>
         </Slide>
         <Slide id="slide2">
