8133e5fd1ae794b17c0863e6ba95cc8c664bc9dd
Sartios Savramis <sartios@ordereze.com>
2017-06-12 20:25:27 +0300

    update components slide

--- src/presentations/reactDay/intro/slides/6/index.js
+++ src/presentations/reactDay/intro/slides/6/index.js
@@ -5,9 +5,9 @@ export default (
     <Slide transition={["zoom"]} bgColor="primary">
       <Heading textColor="tertiary">Components</Heading>
         <List>
-          <Appear><ListItem>Model your app as a components hierarchy</ListItem></Appear>
-          <Appear><ListItem>Each component have props and state</ListItem></Appear>
-          <Appear><ListItem>React Lifecycle hooks</ListItem></Appear>
+            <Appear><ListItem>Design as Component Hierarchy</ListItem></Appear>
+          <Appear><ListItem>Data as props and state</ListItem></Appear>
+          <Appear><ListItem>Workflow as Lifecycle hooks</ListItem></Appear>
         </List>
     </Slide>
 );
