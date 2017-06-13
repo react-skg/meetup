374b9121fdee470abb8dcad80cb968a66dce7b74
Sartios Savramis <sartios@ordereze.com>
2017-06-13 20:16:00 +0300

    refactor slides 2 and 3

--- src/presentations/reactDay/intro/slides/2/index.js
+++ src/presentations/reactDay/intro/slides/2/index.js
@@ -1,28 +1,12 @@
 import React from "react";
-import { Heading, Slide, List, Appear } from "spectacle";
-
-import ListItemControl from '../../components/listItemControl/listItemControl';
+import { Heading, Slide, List, ListItem, Appear } from "spectacle";
 
 export default (
     <Slide transition={["fade"]} bgColor="primary">
-      <Heading>Today we will talk about...</Heading>
-        <List className="slide-2-list-content">
-          <ListItemControl
-            className="fadeInUp animated" animationDelay="1s">
-            What is React JS
-          </ListItemControl>
-          <ListItemControl
-            className="fadeInUp animated" animationDelay="3s">
-            JSX and Virtual DOM
-          </ListItemControl>
-          <ListItemControl
-            className="fadeInUp animated" animationDelay="5s">
-            State & Events
-          </ListItemControl>
-            <ListItemControl
-              className="fadeInUp animated" animationDelay="7s">
-              Going Large Scale with React JS
-            </ListItemControl>
+      <Heading textColor="tertiary">What is React JS</Heading>
+        <List>
+          <Appear><ListItem>Facebook UI library</ListItem></Appear>
+          <Appear><ListItem>Creator: Jordan Walke (@jordwalke)</ListItem></Appear>
         </List>
     </Slide>
 );
--- src/presentations/reactDay/intro/slides/3/index.js
+++ src/presentations/reactDay/intro/slides/3/index.js
@@ -3,10 +3,8 @@ import { Heading, Slide, List, ListItem, Appear } from "spectacle";
 
 export default (
     <Slide transition={["fade"]} bgColor="primary">
-      <Heading textColor="tertiary">What is React JS</Heading>
+      <Heading textColor="tertiary">React Concepts</Heading>
         <List>
-          <Appear><ListItem>Facebook UI library</ListItem></Appear>
-          <Appear><ListItem>Creator: Jordan Walke (@jordwalke)</ListItem></Appear>
           <Appear><ListItem>Components</ListItem></Appear>
           <Appear><ListItem>JSX Converter</ListItem></Appear>
           <Appear><ListItem>Virtual DOM</ListItem></Appear>
