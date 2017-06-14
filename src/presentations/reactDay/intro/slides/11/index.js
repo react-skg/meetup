1d83559444583e89c0e69266f97cd44fd643e237
Sartios Savramis <sartios@ordereze.com>
2017-06-14 23:08:43 +0300

    add animation to all slides of intro

--- src/presentations/reactDay/intro/slides/11/index.js
+++ src/presentations/reactDay/intro/slides/11/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Heading, Slide, List, ListItem, Appear } from "spectacle";
+import { Heading, Slide, List } from "spectacle";
+
+import ListItemControl from '../../components/listItemControl/listItemControl';
 
 export default (
     <Slide transition={["zoom"]} bgColor="primary">
       <Heading textColor="tertiary">Virtual DOM</Heading>
         <List>
-          <Appear><ListItem>DOM: abstraction of HTML</ListItem></Appear>
-          <Appear><ListItem>Virtual DOM: abstraction of DOM</ListItem></Appear>
-          <Appear><ListItem>Compute & apply diffs</ListItem></Appear>
-          <Appear><ListItem>Re render the app</ListItem></Appear>
+          <ListItemControl>DOM: abstraction of HTML</ListItemControl>
+          <ListItemControl>Virtual DOM: abstraction of DOM</ListItemControl>
+          <ListItemControl>Compute & apply diffs</ListItemControl>
+          <ListItemControl>Re render the app</ListItemControl>
         </List>
     </Slide>
 );
