cbc7f66424152ac91fe57cf651b16f6d734cd842
Sartios Savramis <sartios@ordereze.com>
2017-06-14 21:30:30 +0300

    update slide 9

--- src/presentations/reactDay/intro/slides/9/index.js
+++ src/presentations/reactDay/intro/slides/9/index.js
@@ -5,9 +5,9 @@ export default (
     <Slide transition={["zoom"]} bgColor="primary">
       <Heading textColor="tertiary">JSX</Heading>
         <List>
-          <Appear><ListItem>Syntactic sugar for React.createElement</ListItem></Appear>
-          <Appear><ListItem>React must be in scope</ListItem></Appear>
-          <Appear><ListItem>Use Dot Notation</ListItem></Appear>
+          <Appear><ListItem>XML/HTML-like text</ListItem></Appear>
+          <Appear><ListItem>Preprocessors transform to js</ListItem></Appear>
+          <Appear><ListItem>React.createElement()</ListItem></Appear>
         </List>
     </Slide>
 );
