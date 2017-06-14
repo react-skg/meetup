f91240f2121b719da02df1fe1c80a9b00a65c669
Sartios Savramis <sartios@ordereze.com>
2017-06-14 21:46:05 +0300

    update the order of react users

--- src/presentations/reactDay/intro/slides/3/index.js
+++ src/presentations/reactDay/intro/slides/3/index.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { Heading, Slide, List, ListItem, Appear } from "spectacle";
+import { Heading, Slide } from "spectacle";
 
 export default (
     <Slide transition={["fade"]} bgColor="primary">
-      <Heading textColor="tertiary">React Concepts</Heading>
-        <List>
-          <Appear><ListItem>Components</ListItem></Appear>
-          <Appear><ListItem>JSX Converter</ListItem></Appear>
-          <Appear><ListItem>Virtual DOM</ListItem></Appear>
-        </List>
+      <Heading textColor="tertiary">Who is using React?</Heading>
     </Slide>
 );
--- src/presentations/reactDay/intro/slides/4/index.js
+++ src/presentations/reactDay/intro/slides/4/index.js
@@ -1,8 +1,50 @@
 import React from "react";
-import { Heading, Slide } from "spectacle";
+
+import { Slide, Image, Link } from "spectacle";
+
+import './slide5.post.css';
+
+const images = {
+  airbnb: require("../../assets/airbnb.png"),
+  bbc: require("../../assets/bbc.png"),
+  facebook: require("../../assets/facebook.png"),
+  firefox: require("../../assets/firefox.png"),
+  hipchat: require("../../assets/hipchat.png"),
+  instagram: require("../../assets/instagram.png"),
+  khanacademy: require("../../assets/khanacademy.png"),
+  netflix: require("../../assets/netflix.png"),
+  reddit: require("../../assets/reddit.png"),
+  wordpress: require("../../assets/wordpress.png"),
+  yahoo: require("../../assets/yahoo.png"),
+  twitter: require("../../assets/twitter.png"),
+};
 
 export default (
     <Slide transition={["fade"]} bgColor="primary">
-      <Heading textColor="tertiary">Who is using React?</Heading>
+        <div className="intro-presentation-react-users-container">
+          <Image key="1" className="bounceIn animated" src={images.airbnb.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="2" className="bounceIn animated" src={images.bbc.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="3" className="bounceIn animated" src={images.facebook.replace("/", "")} margin="0px auto 40px" height="120px"/>
+        </div>
+        <div className="intro-presentation-react-users-container">
+          <Image key="4" className="bounceIn animated" src={images.firefox.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="5" className="bounceIn animated" src={images.hipchat.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="6" className="bounceIn animated" src={images.twitter.replace("/", "")} margin="0px auto 40px" height="120px"/>
+        </div>
+        <div className="intro-presentation-react-users-container">
+          <Image key="7" className="bounceIn animated" src={images.instagram.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="8" className="bounceIn animated" src={images.khanacademy.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="9" className="bounceIn animated" src={images.netflix.replace("/", "")} margin="0px auto 40px" height="120px"/>
+        </div>
+        <div className="intro-presentation-react-users-container">
+          <Image key="10" className="bounceIn animated" src={images.reddit.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="11" className="bounceIn animated" src={images.wordpress.replace("/", "")} margin="0px auto 40px" height="120px"/>
+          <Image key="12" className="bounceIn animated" src={images.yahoo.replace("/", "")} margin="0px auto 40px" height="120px"/>
+        </div>
+        <div className="intro-presentation-react-users-link bounceIn animated">
+          <Link
+            href="https://github.com/facebook/react/wiki/sites-using-react"
+            target="_blank">More React Users</Link>
+        </div>
     </Slide>
 );
