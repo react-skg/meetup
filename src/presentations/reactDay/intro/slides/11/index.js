f4df8055dbc7d73d875482d7f42624eaffda0c26
Sartios Savramis <sartios@ordereze.com>
2017-06-14 22:22:03 +0300

    update virtual dom content

--- src/presentations/reactDay/intro/slides/11/index.js
+++ src/presentations/reactDay/intro/slides/11/index.js
@@ -5,10 +5,9 @@ export default (
     <Slide transition={["zoom"]} bgColor="primary">
       <Heading textColor="tertiary">Virtual DOM</Heading>
         <List>
-          <Appear><ListItem>DOM: abstraction of a structured text</ListItem></Appear>
-          <Appear><ListItem>Virtual DOM: abstraction of the HTML DOM</ListItem></Appear>
-          <Appear><ListItem>React computes diffs using Virtual DOM</ListItem></Appear>
-          <Appear><ListItem>React applies diffs to DOM</ListItem></Appear>
+          <Appear><ListItem>DOM: abstraction of HTML</ListItem></Appear>
+          <Appear><ListItem>Virtual DOM: abstraction of DOM</ListItem></Appear>
+          <Appear><ListItem>Compute & apply diffs</ListItem></Appear>
           <Appear><ListItem>Re render the app</ListItem></Appear>
         </List>
     </Slide>
